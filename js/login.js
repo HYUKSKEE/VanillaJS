4bb5a9353f886dd126774444a59c2b1e9d026580
HYUKSKEE <gin280833@gmail.com>
2022-04-18 18:31:49 +0900

    04/18 VanillaJS

--- js/login.js
+++ js/login.js
@@ -1,3 +1,4 @@
+"use strict";
 const loginForm = document.querySelector("#Login-form");
 const loginInput = document.querySelector("#Login-form input");
 const goToLink = document.querySelector("#link");
@@ -6,22 +7,22 @@ const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username);
-  if (savedUsername != "hyukskee") {
+  if (username != "hyukskee") {
     event.preventDefault();
     console.log(`null or error`);
   } else {
     event.preventDefault();
+    localStorage.setItem(USERNAME_KEY, username);
     loginForm.classList.add(HIDDEN_CLASSNAME);
     goToLink.classList.remove(HIDDEN_CLASSNAME);
   }
 }
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-if (savedUsername == null) {
+if (savedUsername === null) {
   // show the form
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
-} else {
+} else if (savedUsername == "hyukskee") {
   // show the greeting
   loginForm.classList.add(HIDDEN_CLASSNAME);
   goToLink.classList.remove(HIDDEN_CLASSNAME);
